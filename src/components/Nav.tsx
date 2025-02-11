ce69a951dcc74298a9004e0fd4f38ea6189347f6
gilmerperez <gilmer2002@outlook.com>
2025-02-11 10:10:26 -0500

    Added Navigation Links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
+  const currentPage = useLocation().pathname;
+
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <nav className="nav nav-pills justify-content-center p-3 bg-light">
+      <ul className="nav nav-pills">
+        <li className="nav-item"><Link to="/" className={currentPage === "/" ? "nav-link active" : "nav-link"}>Candiate Search</Link></li>
+        <li className="nav-item"><Link to="/SavedCandidates" className={currentPage === "/SavedCandidates" ? "nav-link active" : "nav-link"}>Saved Candidates</Link></li>
+      </ul>
+    </nav>
   )
 };
 
