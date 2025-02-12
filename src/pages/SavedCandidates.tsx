e2f567e9cafca25ae92426501af234e33f768938
gilmerperez <gilmer2002@outlook.com>
2025-02-11 22:27:36 -0500

    Added way to display non-existing data in SavedCandidates.tsx

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -23,7 +23,7 @@ const SavedCandidates = () => {
   }, []);
 
   // Remove candidate function
-  const removeCandidate = (login: unknown) => {
+  const removeCandidate = (login: string) => {
     const updatedCandidates = savedCandidates.filter((candidate) => candidate.login !== login);
     setSavedCandidates(updatedCandidates);
     localStorage.setItem("savedCandidates", JSON.stringify(updatedCandidates));
@@ -37,26 +37,26 @@ const SavedCandidates = () => {
         <table>
           <thead>
             <tr>
+              <th>Username</th>
               <th>Name</th>
-              <th>login</th>
-              <th>Location</th>
               <th>Avatar</th>
+              <th>Location</th>
               <th>Email</th>
               <th>Company</th>
+              <th>GitHub Profile</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {savedCandidates.map((candidate) => (
               <tr key={candidate.login}>
+                <td>{candidate.login || "N/A"}</td>
                 <td>{candidate.name || "N/A"}</td>
-                <td>{candidate.login}</td>
+                <td><img src={candidate.avatar_url || "N/A"} alt="Avatar" width={50} height={50} /></td>
                 <td>{candidate.location || "N/A"}</td>
-                <td>
-                  <img src={candidate.avatar_url} alt="Avatar" width={50} height={50} />
-                </td>
                 <td>{candidate.email || "N/A"}</td>
                 <td>{candidate.company || "N/A"}</td>
+                <td>{candidate.html_url || "N/A"}</td>
                 <td>
                   <button onClick={() => removeCandidate(candidate.login)}>Remove</button>
                 </td>
